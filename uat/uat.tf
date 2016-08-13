7fbb2acee497556d01e0869ab57d77d82e24a3e3
Wilson Carey <carey@unif.io>
2016-08-13 09:13:20 -0400

    Extend UAT testing

--- uat/uat.tf
+++ uat/uat.tf
@@ -1,3 +1,13 @@
 # Terraform verification
 
 provider "aws" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.100.0.0/20"
+
+  tags {
+    Name        = "terraform-container-uat"
+    application = "circleci"
+    managed_by  = "terraform"
+  }
+}
