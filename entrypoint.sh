688425f07b6cf57e94b47b97aa05222b3bc48543
Wilson Carey <carey@unif.io>
2016-10-01 15:18:26 -0400

    Added gosu and entryppoint script for optionally changing user

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## Defaulting to root user (UID 0) to maintain backwards compatibility
+USER_ID=${LOCAL_USER_ID:-0}
+
+if [ "$DEBUG" == true ] ; then
+  echo "Starting with UID: $USER_ID"
+fi
+if [ "$USER_ID" -ne "0" ]; then
+  adduser -s /bin/sh -D -u $USER_ID user
+  export HOME=/home/user
+  exec /bin/gosu user /bin/terraform "$@"
+else
+  exec /bin/terraform "$@"
+fi
